d34bdf12fa953553f70fe2c86711817c9c8daab6
Melanie <contact.mriche@gmail.com>
2021-06-09 13:54:01 +0200

    Getting Sci-Fi Movie et Passing Props to Card

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,7 +1,32 @@
-const Section = ({ genre }) => {
+import { useEffect, useState } from 'react'
 
+import Card from './Card'
+
+const Section = ({ genre }) => {
+  const [movies, setMovies] = useState(null)
+
+  const fetchData = async () => {
+    const response = await fetch("/.netlify/functions/getMovies")
+    const responseBody = await response.json()
+    setMovies(responseBody.data.movies_by_genre.values)
+
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+  console.log(movies)
   return (
+    <>
     <div>{genre}</div>
+    {movies && (
+      <div className="movie-section">
+        {movies.map((movie, index) => (
+          <Card key={index} movie={movie}/>
+        ))}
+      </div>
+    )}
+    </>
   )
 }
 
